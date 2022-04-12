44aa8f273b38daa5d8b067d9e15eaca46bdf0945
patel-zeel <patel_zeel@iitgn.ac.in>
2022-04-12 21:38:23 +0530

    add other utils

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,5 +2,16 @@ import pytest
 
 
 def test_import():
+    # import the savefig and latexify modules
     import probml_utils
     from probml_utils import latexify, savefig
+
+    # import other files
+    from probml_utils import fisher_lda_fit
+    from probml_utils import gauss_utils
+    from probml_utils import gmm_lib
+    from probml_utils import mix_bernoulli_lib
+    from probml_utils import plotting
+    from probml_utils import prefit_voting_classifier
+    from probml_utils import rvm_classifier
+    from probml_utils import rvm_regressor
