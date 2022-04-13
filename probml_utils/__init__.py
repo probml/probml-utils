2d3468707e1b84be10972663fc7b7716196c82f2
patel-zeel <patel_zeel@iitgn.ac.in>
2022-04-13 14:24:00 +0530

    add `pyprobml_utils` and tests

--- probml_utils/__init__.py
+++ probml_utils/__init__.py
@@ -1,2 +1,10 @@
 from ._version import version as __version__
 from .plotting import savefig, latexify
+from .pyprobml_utils import (
+    hinton_diagram,
+    plot_ellipse,
+    convergence_test,
+    kdeg,
+    scale_3d,
+    style3d,
+)
--- tests/test_import.py
+++ tests/test_import.py
@@ -1,18 +1,27 @@
 import pytest
+import probml_utils as pml
 
 
-def test_import():
-    # import the savefig and latexify modules
-    import probml_utils
-    from probml_utils import latexify, savefig
+def test_import_scripts():
+    # Files
+    pml.__version__
+    pml.savefig
+    pml.latexify
+    pml.hinton_diagram
+    pml.plot_ellipse
+    pml.convergence_test
+    pml.kdeg
+    pml.scale_3d
+    pml.style3d
 
-    # import other files
+
+def test_import_modules():
     from probml_utils import fisher_lda_fit
     from probml_utils import gauss_utils
     from probml_utils import gmm_lib
-    from probml_utils import hinton_diagram
     from probml_utils import mix_bernoulli_lib
+    from probml_utils import mixture_lib
     from probml_utils import plotting
     from probml_utils import prefit_voting_classifier
-    from probml_utils import rvm_classifier
-    from probml_utils import rvm_regressor
+    from probml_utils import pyprobml_utils
+    from probml_utils import rvm_classifier, rvm_regressor
