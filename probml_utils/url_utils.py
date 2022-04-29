1b0f4565a4b3119243e58ce930797406ddc9975f
karm-patel <karmpatel216@gmail.com>
2022-04-29 18:33:05 +0530

    added `github_url_to_colab_url` in `url_utils`

--- probml_utils/url_utils.py
+++ probml_utils/url_utils.py
@@ -29,3 +29,37 @@ def check_dead_urls(urls: Any, print_dead_url=False):
     if print_dead_url:
         print(f"{cnt} dead urls detected!")
     return mapping_treedef.unflatten(status) # convert to original structure
+
+def github_url_to_colab_url(url):
+    '''
+    convert github .ipynb url to colab .ipynb url
+    '''
+    if not (url.startswith("https://github.com")):
+        print("INVALID URL: not a Github url")
+        return 0
+    
+    if not(url.endswith(".ipynb")):
+        print("INVALID URL: not a .ipynb file")
+        return 0
+    
+    base_url_colab = "https://colab.research.google.com/github/"
+    base_url_github = "https://github.com/"
+    
+    return url.replace(base_url_github, base_url_colab)
+
+def colab_url_to_github_url(url):
+    '''
+    convert colab .ipynb url to github .ipynb url
+    '''
+    if not (url.startswith("https://colab.research.google.com/github")):
+        print("INVALID URL: not a colab github url")
+        return 0
+    
+    if not(url.endswith(".ipynb")):
+        print("INVALID URL: not a .ipynb file")
+        return 0
+    
+    base_url_colab = "https://colab.research.google.com/github/"
+    base_url_github = "https://github.com/"
+    
+    return url.replace(base_url_colab,base_url_github)
--- tests/test_url_utils.py
+++ tests/test_url_utils.py
@@ -1,4 +1,4 @@
-from probml_utils.url_utils import check_dead_urls
+from probml_utils.url_utils import check_dead_urls, github_url_to_colab_url, colab_url_to_github_url
 
 def test_dead_urls():
     links = {'1.3': 'https://github.com/probml/pyprobml/blob/master/notebooks/book1/01/iris_plot.ipynb',
@@ -9,3 +9,21 @@ def test_dead_urls():
     status = check_dead_urls(links)
     status_true = {'1.3': 0, '1.4': 0, '1.5': 1, '1.6': 1}
     assert (status == status_true)
+
+def test_github_to_colab():
+    links = {
+        "https://github.com/probml/pyprobml/blob/master/notebooks/book1/13/mlp_1d_regression_hetero_tfp.ipynb":"https://colab.research.google.com/github/probml/pyprobml/blob/master/notebooks/book1/13/mlp_1d_regression_hetero_tfp.ipynb",
+        "https://google.com/probml/pyprobml/blob/master/notebooks/book1/13/mlp_1d_regression_hetero_tfp.ipynb":0,
+        "https://github.com/probml/pyprobml/blob/master/notebooks/book1/13/mlp_1d_regression_hetero_tfp.py":0
+    }
+    for link in links:
+        assert links[link] == github_url_to_colab_url(link)
+
+def test_colab_to_github():
+    links = {
+        "https://colab.research.google.com/github/probml/pyprobml/blob/master/notebooks/book1/13/mlp_1d_regression_hetero_tfp.ipynb":"https://github.com/probml/pyprobml/blob/master/notebooks/book1/13/mlp_1d_regression_hetero_tfp.ipynb",
+        "https://github.research.google.com/github/probml/pyprobml/blob/master/notebooks/book1/13/mlp_1d_regression_hetero_tfp.ipynb":0,
+        "https://colab.research.google.com/github/probml/pyprobml/blob/master/notebooks/book1/13/mlp_1d_regression_hetero_tfp.txt":0
+    }
+    for link in links:
+        assert links[link] == colab_url_to_github_url(link)
