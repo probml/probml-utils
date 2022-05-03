f73596efc5bbc46581071df71b8017c500732563
Karm Patel <59387624+karm-patel@users.noreply.github.com>
2022-05-04 00:35:42 +0530

    Add firestore helper functions in url_utils.py (#8)
    * add `mix_bernoulli_em_mnist.py`
    
    * removed `main`
    
    * Add firestore helper functions in `url_utils.py`

--- probml_utils/url_utils.py
+++ probml_utils/url_utils.py
@@ -1,6 +1,11 @@
 import jax
 import requests
 from typing import Any
+from TexSoup import TexSoup
+import regex as re
+import os
+import pandas as pd
+from firebase_admin import credentials, firestore, initialize_app
 
 def is_dead_url(link):
     '''
@@ -11,7 +16,7 @@ def is_dead_url(link):
         return 1
     return 0
 
-def check_dead_urls(urls: Any, print_dead_url=False):
+def check_dead_urls(urls: Any, print_dead_url=True):
     '''
     returns if urls are dead or not
     '''
@@ -59,3 +64,116 @@ def colab_url_to_github_url(url):
     base_url_github = "https://github.com/"
     
     return url.replace(base_url_colab,base_url_github)
+
+def extract_scripts_name_from_caption(caption):
+    """
+    extract foo.py from ...{https//:<path/to/>foo.py}{foo.py}...
+    """
+    py_pattern = r"\{\S+?\.py\}"
+    ipynb_pattern = r"\{\S+?\.ipynb\}"
+
+    matches = re.findall(py_pattern, str(caption)) + re.findall(ipynb_pattern, str(caption))
+    extracted_scripts = []
+    for each in matches:
+        if "https" not in each:
+            each = each.replace("{", "").replace("}", "").replace("\\_", "_")
+            extracted_scripts.append(each)
+    return extracted_scripts
+
+
+def make_url_from_fig_no_and_script_name(
+    fig_no, script_name, base_url="https://github.com/probml/pyprobml/blob/master/notebooks", book_no=1, convert_to_colab_url = True
+):
+    """
+    create mapping between fig_no and actual_url path
+    (fig_no=1.3,script_name=iris_plot.ipynb) converted to https://github.com/probml/pyprobml/blob/master/notebooks/book1/01/iris_plot.ipynb
+    """
+    chapter_no = int(fig_no.strip().split(".")[0])
+    base_url_ipynb = os.path.join(base_url, f"book{book_no}/{chapter_no:02d}")
+    if ".py" in script_name:
+        script_name = script_name[:-3] + ".ipynb"
+    if convert_to_colab_url:
+        return github_url_to_colab_url(os.path.join(base_url_ipynb, script_name))
+    return os.path.join(base_url_ipynb, script_name)
+
+
+def make_url_from_chapter_no_and_script_name(
+    chapter_no, script_name, base_url="https://github.com/probml/pyprobml/blob/master/notebooks", book_no=1, convert_to_colab_url = True
+):
+    """
+    create mapping between chapter_no and actual_url path
+    (chapter_no = 3,script_name=iris_plot.ipynb) converted to https://github.com/probml/pyprobml/blob/master/notebooks/book1/01/iris_plot.ipynb
+    """
+    base_url_ipynb = os.path.join(base_url, f"book{book_no}/{int(chapter_no):02d}")
+    if ".py" in script_name:
+        script_name = script_name[:-3] + ".ipynb"
+    if convert_to_colab_url:
+        return github_url_to_colab_url(os.path.join(base_url_ipynb, script_name))
+    return os.path.join(base_url_ipynb, script_name)
+
+
+def dict_to_csv(key_value_dict, csv_name):    
+    df = pd.DataFrame(key_value_dict.items(), columns=["key", "url"])
+    df.set_index(keys=["key"], inplace=True, drop=True)
+    df.to_csv(csv_name)
+
+
+def figure_url_mapping_from_lof(lof_file_path, csv_name, convert_to_colab_url = True, base_url = "https://github.com/probml/pyprobml/blob/master/notebooks", book_no=1):
+    with open(lof_file_path) as fp:
+        LoF_File_Contents = fp.read()
+    soup = TexSoup(LoF_File_Contents)
+    
+    # create mapping of fig_no to list of script_name
+    
+    url_mapping = {}
+    for caption in soup.find_all("numberline"):
+        fig_no = str(caption.contents[0])
+        extracted_scripts = extract_scripts_name_from_caption(str(caption))
+        if len(extracted_scripts) == 1:
+            url_mapping[fig_no] = make_url_from_fig_no_and_script_name(fig_no, extracted_scripts[0], convert_to_colab_url=convert_to_colab_url, base_url=base_url, book_no=book_no)
+        elif len(extracted_scripts) > 1:
+            url_mapping[fig_no] = make_url_from_fig_no_and_script_name(fig_no, "fig_"+fig_no.replace(".","_")+".ipynb", convert_to_colab_url= convert_to_colab_url,  base_url=base_url, book_no=book_no)
+    
+    if csv_name:
+        dict_to_csv(url_mapping,csv_name)
+    print(f"Mapping of {len(url_mapping)} urls is saved in {csv_name}")
+    return url_mapping
+
+
+def non_figure_notebook_url_mapping(notebooks_path, csv_name):
+    url_mapping = {}
+    for notebook_path in notebooks_path:
+        parts = notebook_path.split("/")
+        script_name = parts[-1]
+        chapter_no = parts[-2]
+        book_no = parts[-3]
+        url = make_url_from_chapter_no_and_script_name(chapter_no,script_name)
+        key = script_name.split(".")[0] # remove extension
+        url_mapping[key] = url
+    if csv_name:
+        dict_to_csv(url_mapping,csv_name)
+    print(f"Mapping of {len(url_mapping)} urls is saved in {csv_name}")
+    return url_mapping
+
+def upload_urls_to_firestore(key_path,csv_path,level1_collection = "figures",
+                             level2_document = None ,level3_collection = None):
+    
+    assert level2_document in ["book1", "book2"], "Incorrect level2_document value: possible values of level2_document should be ['book1', 'book2']"
+
+    cred = credentials.Certificate(key_path)
+    try:
+        default_app = initialize_app(cred) #this should called only once
+    except ValueError:
+        pass
+    db = firestore.client()
+
+    collection = db.collection(level1_collection).document(level2_document).collection(level3_collection)
+    
+    df = pd.read_csv(csv_path)
+    
+    assert sorted(df.columns) ==  ["key","url"], f"columns of {csv_path} should be only 'key' and 'url'" 
+    
+    print("Uploading...")
+    for (key,url) in list(zip(df["key"], df["url"])):
+        collection.document(str(key)).set({"link": url})
+    print(f"{len(df)} urls uploaded!")
