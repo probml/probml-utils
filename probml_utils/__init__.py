5763fc0c1f58a0f7723cf1fa503135582088678e
karm-patel <karmpatel216@gmail.com>
2022-06-17 17:23:22 +0530

    Arviz trace method added in `blackjax_utils`

--- probml_utils/__init__.py
+++ probml_utils/__init__.py
@@ -1,5 +1,6 @@
 from ._version import version as __version__
 from .plotting import savefig, latexify, _get_fig_name, is_latexify_enabled
+from .blackjax_utils import arviz_trace_from_states
 from .pyprobml_utils import (
     hinton_diagram,
     plot_ellipse,
