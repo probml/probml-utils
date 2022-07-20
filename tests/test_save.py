bf44e94017f10cd7160f00cf1e3197191c6b646f
patel-zeel <patel_zeel@iitgn.ac.in>
2022-07-20 10:18:53 +0530

    update `test_save`

--- tests/test_save.py
+++ tests/test_save.py
@@ -1,20 +1,25 @@
+import pytest
+
 import os
 import probml_utils as pml
 import matplotlib.pyplot as plt
 
 
-def test_save_latexified():
+@pytest.mark.parametrize("latexify", [True, False])
+def test_save(latexify):
+    if latexify:
+        os.environ["LATEXIFY"] = ""
+        suffix = "_latexified"
+    else:
+        if "LATEXIFY" in os.environ:
+            os.environ.pop("LATEXIFY")
+        suffix = ""
+
     os.environ["FIG_DIR"] = "figures"
-    os.environ["LATEXIFY"] = ""
     pml.latexify(width_scale_factor=2, fig_height=1.5)
     plt.plot([1.0, 2.0], [3.0, 4.0])
+    save_name = os.path.join(os.environ["FIG_DIR"], f"test{suffix}.pdf")
+    if os.path.exists(save_name):
+        os.remove(save_name)
     pml.savefig("test")
-    assert os.path.exists("figures/test_latexified.pdf")
-    
-def test_save_normal():
-    if "LATEXIFY" in os.environ:
-        os.environ.pop("LATEXIFY")
-    os.environ["FIG_DIR"] = "figures"
-    plt.plot([1.0, 2.0], [3.0, 4.0])
-    pml.savefig("test")
-    assert os.path.exists("figures/test.png")
+    assert os.path.exists(save_name)
