f01472f95d9e0e4aee6f3cafd1826839fc6955d6
Qingyao Sun <sunqingyao19970825@icloud.com>
2022-07-19 20:27:47 -0500

    rename conv_vae_utils.py to conv_vae_flax_utils.py

--- probml_utils/conv_vae_flax_utils.py
+++ probml_utils/conv_vae_flax_utils.py
@@ -1,34 +1,25 @@
 from typing import Tuple, Sequence
 from functools import partial
-import os
 
 import jax
 import jax.numpy as jnp
 import numpy as np
 
-try:
-    import flax
-except ModuleNotFoundError:
-    os.system("pip install -qq flax")
-    import flax
+import flax
 import flax.linen as nn
 from flax.training import train_state
 
-try:
-    import optax
-except ModuleNotFoundError:
-    os.system("pip install -qq optax")
-    import optax
+import optax
 
 
 class Encoder(nn.Module):
     latent_dim: int
-    hidden_dims: Sequence[int] = (32, 64, 128, 256, 512)
+    hidden_channels: Sequence[int]
 
     @nn.compact
     def __call__(self, X, training):
-        for hidden_dim in self.hidden_dims:
-            X = nn.Conv(hidden_dim, (3, 3), strides=2, padding=1)(X)
+        for channel in self.hidden_channels:
+            X = nn.Conv(channel, (3, 3), strides=2, padding=1)(X)
             X = nn.BatchNorm(use_running_average=not training)(X)
             X = jax.nn.relu(X)
 
@@ -41,23 +32,27 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
     output_dim: Tuple[int, int, int]
-    hidden_dims: Sequence[int] = (32, 64, 128, 256, 512)
+    hidden_channels: Sequence[int]
 
     @nn.compact
     def __call__(self, X, training):
         H, W, C = self.output_dim
 
         # TODO: relax this restriction
-        factor = 2**len(self.hidden_dims)
-        assert H % factor == W % factor == 0, f"output_dim must be a multiple of {factor}"
+        factor = 2 ** len(self.hidden_channels)
+        assert (
+            H % factor == W % factor == 0
+        ), f"output_dim must be a multiple of {factor}"
         H, W = H // factor, W // factor
 
-        X = nn.Dense(H * W * self.hidden_dims[-1])(X)
+        X = nn.Dense(H * W * self.hidden_channels[-1])(X)
         X = jax.nn.relu(X)
-        X = X.reshape((-1, H, W, self.hidden_dims[-1]))
+        X = X.reshape((-1, H, W, self.hidden_channels[-1]))
 
-        for hidden_dim in reversed(self.hidden_dims[:-1]):
-            X = nn.ConvTranspose(hidden_dim, (3, 3), strides=(2, 2), padding=((1, 2), (1, 2)))(X)
+        for hidden_channel in reversed(self.hidden_channels[:-1]):
+            X = nn.ConvTranspose(
+                hidden_channel, (3, 3), strides=(2, 2), padding=((1, 2), (1, 2))
+            )(X)
             X = nn.BatchNorm(use_running_average=not training)(X)
             X = jax.nn.relu(X)
 
@@ -77,10 +72,11 @@ class VAE(nn.Module):
     variational: bool
     latent_dim: int
     output_dim: Tuple[int, int, int]
+    hidden_channels: Sequence[int]
 
     def setup(self):
-        self.encoder = Encoder(self.latent_dim)
-        self.decoder = Decoder(self.output_dim)
+        self.encoder = Encoder(self.latent_dim, self.hidden_channels)
+        self.decoder = Decoder(self.output_dim, self.hidden_channels)
 
     def __call__(self, key, X, training):
         mean, logvar = self.encoder(X, training)
@@ -101,11 +97,15 @@ class TrainState(train_state.TrainState):
     beta: float
 
 
-def create_train_state(key, variational, beta, latent_dim, learning_rate, specimen):
-    vae = VAE(variational, latent_dim, specimen.shape)
+def create_train_state(
+    key, variational, beta, latent_dim, hidden_channels, learning_rate, specimen
+):
+    vae = VAE(variational, latent_dim, specimen.shape, hidden_channels)
     key_dummy = jax.random.PRNGKey(42)
     (recon, _, _), variables = vae.init_with_output(key, key_dummy, specimen, True)
-    assert recon.shape[-3:] == specimen.shape, f"{recon.shape} = recon.shape != specimen.shape = {specimen.shape}"
+    assert (
+        recon.shape[-3:] == specimen.shape
+    ), f"{recon.shape} = recon.shape != specimen.shape = {specimen.shape}"
     tx = optax.adam(learning_rate)
     state = TrainState.create(
         apply_fn=vae.apply,
@@ -120,7 +120,7 @@ def create_train_state(key, variational, beta, latent_dim, learning_rate, specim
 
 @jax.vmap
 def kl_divergence(mean, logvar):
-  return -0.5 * jnp.sum(1 + logvar - jnp.square(mean) - jnp.exp(logvar))
+    return -0.5 * jnp.sum(1 + logvar - jnp.square(mean) - jnp.exp(logvar))
 
 
 @jax.jit
@@ -128,13 +128,19 @@ def train_step(state, key, image):
     @partial(jax.value_and_grad, has_aux=True)
     def loss_fn(params):
         variables = {"params": params, "batch_stats": state.batch_stats}
-        (recon, mean, logvar), new_model_state = state.apply_fn(variables, key, image, True, mutable=["batch_stats"])
-        loss = jnp.sum((recon - image) ** 2) + state.beta * jnp.sum(kl_divergence(mean, logvar))
+        (recon, mean, logvar), new_model_state = state.apply_fn(
+            variables, key, image, True, mutable=["batch_stats"]
+        )
+        loss = jnp.sum((recon - image) ** 2) + state.beta * jnp.sum(
+            kl_divergence(mean, logvar)
+        )
         return loss.sum(), new_model_state
 
     (loss, new_model_state), grads = loss_fn(state.params)
 
-    state = state.apply_gradients(grads=grads, batch_stats=new_model_state["batch_stats"])
+    state = state.apply_gradients(
+        grads=grads, batch_stats=new_model_state["batch_stats"]
+    )
 
     return state, loss
 
