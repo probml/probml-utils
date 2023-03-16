e28f700bbdf468ac26cc081e59cc26fad2f8a6d1
Kevin Murphy <murphyk@gmail.com>
2023-03-16 01:10:54 +0000

    extend setup

--- paperspace-setup.sh
+++ paperspace-setup.sh
@@ -7,15 +7,17 @@
 
 # Upgrade Pytorch and TF and JAX
 
-pip3 install --upgrade torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+#pip3 install --upgrade torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 
-pip3 install --upgrade --user tensorflow tensorflow_probability
+#pip3 install --upgrade --user tensorflow tensorflow_probability
 
 pip3 install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 # jax Libraries
 
-pip3 install distrax optax
+pip3 install distrax optax flax chex
+pip3 install -Uq tfp-nightly[jax]
+# https://www.tensorflow.org/probability/examples/TensorFlow_Probability_on_JAX
 
 # github
 git config --global user.email "murphyk@gmail.com"
@@ -23,7 +25,11 @@ git config --global user.name "Kevin Murphy"
 
 sudo snap install gh
 
-gh auth login  # paste personal access token
+#gh auth login  # paste personal access token
+
+# avoid having to paste PAT more than once per day
+# https://git-scm.com/book/en/v2/Git-Tools-Credential-Storage
+git config --global credential.helper 'cache --timeout 90000'
 
 # Other stuff
 pip3 install seaborn tdqm
