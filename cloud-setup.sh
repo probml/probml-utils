01efd592b06730341aff7171fda9a8407bbf6a05
Kevin Murphy <murphyk@gmail.com>
2023-03-16 16:43:23 +0000

    simplify setup

--- cloud-setup.sh
+++ cloud-setup.sh
@@ -1,6 +1,6 @@
-# modified from
+# Cloud VM setup. Modified from
 # https://github.com/Paperspace/ml-in-a-box/blob/main/ml_in_a_box.sh
-# Also works for lambdalabs
+# Also works for lambdalabs and TPU VM
 
 #!/usr/bin/env bash
 
@@ -9,18 +9,20 @@
 
 pip3 install torch torchvision torchaudio
 
+# for pytorch on TPU V4
+export TPU_NUM_DEVICES=4
+
 #pip3 install --upgrade --user tensorflow tensorflow_probability
 
 pip3 install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 # jax Libraries
-
-pip3 install distrax optax flax chex einops
+pip3 install distrax optax flax chex einops jaxtyping jax-tqdm
 pip3 install -Uq tfp-nightly[jax]
 # https://www.tensorflow.org/probability/examples/TensorFlow_Probability_on_JAX
 
-
-pip3 install seaborn tdqm scikit-learn jax-tqdm
+# other libraries
+pip3 install seaborn tdqm scikit-learn 
 
 # github
 git config --global user.email "murphyk@gmail.com"
@@ -35,5 +37,17 @@ git config --global credential.helper 'cache --timeout 90000'
 #gh auth login  # paste personal access token
 
 
+# Install mamba
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+bash Mambaforge-$(uname)-$(uname -m).sh
+# Restart bash instance 
+#exec bash
 
-
+mamba install -y jupyterlab
+mamba install -y nodejs
+mamba install -y seaborn
+mamba install -y jupyter
+pip install --upgrade jupyterlab-git
+sudo snap install tree
+mamba install dask
+#jupyter labextension install base16-mexico-light
